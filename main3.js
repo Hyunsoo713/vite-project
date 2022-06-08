529d55f37d096661d57393b92dcff34a5636c26a
Hyunsoo713 <hyun91998@snu.ac.kr>
2022-06-08 19:53:39 +0900

    hi

--- main3.js
+++ main3.js
@@ -1,27 +1,28 @@
 import './style3.scss';
 import './reset.scss';
 
+
 function changeImage(){
-document.getElementById("#hyunsoo").src="./1x/Asset.png";  
+document.getElementsByClassName(".artist-page-1").src="./a.png";  
 }
 
-var target = document.querySelectorAll('.artist-page');
+var target = document.querySelectorAll('.artist-page-1');
 var targetID;
 
 // 팝업 열기
 
 for(var i = 0; i < target.length; i++){
   target[i].addEventListener('click', function(e){
-    console.log(e.target.id);
-    document.getElementById('popup').style.display = 'block';
+   // console.log(e.target.id);
+    document.getElementById('popup-1').style.display = 'block';
   });
 }
 
 // 팝업 닫기
-for(var j = 0; j < target.length; j++){
-  btnPopClose[j].addEventListener('click', function(){
-    this.parentNode.parentNode.style.display = 'none';
-  });
-}
+//for(var j = 0; j < target.length; j++){
+  //btnPopClose[j].addEventListener('click', function(){
+    //this.parentNode.parentNode.style.display = 'none';
+  //});
+//}
 
 

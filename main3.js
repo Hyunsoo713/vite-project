74119c06967b6ccd26336f629ec8476dddc925c8
Hyunsoo713 <hyun91998@snu.ac.kr>
2022-06-18 12:15:09 +0900

    jojo

--- main3.js
+++ main3.js
@@ -1,6 +1,5 @@
 import './style3.scss';
 import './reset.scss';
-
 var target = document.querySelectorAll('.artist-page');
 console.log(target)
 
@@ -8,10 +7,8 @@ console.log(target)
 
 for(var i = 0; i < target.length; i++){
   target[i].addEventListener('click', function(e){
-   // console.log($(`#popup-${i}`)[0])
-    //console.log(e.target.className)
-    $('.popup')[0].style.display = 'flex';
 
+    $('.popup')[0].style.display = 'flex';
     if (e.target.className.includes('1')) {
       $(`#popup-1`)[0].style.display = 'block';
     } else if(e.target.className.includes('2')) {
